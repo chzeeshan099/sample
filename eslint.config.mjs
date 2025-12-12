b688cddaa3410eed0b5227597eecdd134eae802d
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2025-12-12 08:47:40 +0500

    fix issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+  {
     ignores: ["node_modules", ".next"],
   },
 ];
